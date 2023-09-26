11ea69283e1a90b19b044f0c308b24b04d168ce0
BT-Matshazi <bekithembamatshazi@gmail.com>
2023-09-26 19:55:56 +0200

    view sightings fix by Kiyan

--- app/src/main/java/byrd/syrch/SightingsListAdapter.kt
+++ app/src/main/java/byrd/syrch/SightingsListAdapter.kt
@@ -2,40 +2,45 @@ package byrd.syrch
 
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import byrd.syrch.databinding.FragmentSightingsListBinding
+import android.widget.TextView
+import byrd.syrch.birdData.SightingData
+import byrd.syrch.databinding.FragmentSightingsItemListBinding
 import byrd.syrch.placeholder.PlaceholderContent
 
 /**
  * [RecyclerView.Adapter] that can display a [PlaceholderItem].
  * TODO: Replace the implementation with code for your data type.
  */
-class SightingsListAdapter(
-    private val values: List<PlaceholderContent.RareBirds>
-) : RecyclerView.Adapter<SightingsListAdapter.ViewHolder>() {
+class SightingsListAdapter(private val values: ArrayList<SightingData>) : RecyclerView.Adapter<SightingsListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-        return ViewHolder(
-            FragmentSightingsListBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.sightings_item, parent, false)
+        return ViewHolder(itemView)
     }
 
+
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         val item = values[position]
+
+        holder.birdName.text = item.birdName
+        holder.sightingDate.text = item.dateAdded
+        holder.sightingLocation.text = item.location
     }
 
     override fun getItemCount(): Int = values.size
 
-    inner class ViewHolder(binding: FragmentSightingsListBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val birdName: TextView = itemView.findViewById(R.id.birdName)
+        val sightingDate: TextView = itemView.findViewById(R.id.sightingDate)
+        val sightingLocation: TextView = itemView.findViewById(R.id.sightingLocation)
 
         override fun toString(): String {
-            return super.toString() + " '"  + "'"
+            return super.toString() + " '" + "'"
         }
     }
+
 }
